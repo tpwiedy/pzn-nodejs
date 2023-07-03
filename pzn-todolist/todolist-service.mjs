8b05b7fe51cbcc511737aafebb268ca1b5aedd72
tpwiedy <wiedy.pratama@gmail.com>
2023-07-03 23:57:37 +0700

    Created simple todolist API

---
+++ pzn-todolist/todolist-service.mjs
@@ -0,0 +1,53 @@
+export class TodolistService {
+  todolist = ["Programmer", "Zaman", "Now"];
+
+  getJsonTodoList() {
+    return JSON.stringify({
+      code: 200,
+      status: "OK",
+      data: this.todolist.map((value, index) => {
+        return {
+          id: index,
+          todo: value,
+        };
+      }),
+    });
+  }
+
+  getTodoList(req, res) {
+    res.write(this.getJsonTodoList());
+    res.end();
+  }
+
+  createTodo(req, res) {
+    req.addListener("data", (data) => {
+      const body = JSON.parse(data.toString());
+      this.todolist.push(body.todo);
+
+      res.write(this.getJsonTodoList());
+      res.end();
+    });
+  }
+
+  updateTodo(req, res) {
+    req.addListener("data", (data) => {
+      const body = JSON.parse(data.toString());
+      if (this.todolist[body.id]) {
+        this.todolist[body.id] = body.todo;
+      }
+      res.write(this.getJsonTodoList());
+      res.end();
+    });
+  }
+
+  deleteTodo(req, res) {
+    req.addListener("data", (data) => {
+      const body = JSON.parse(data.toString());
+      if (this.todolist[body.id]) {
+        this.todolist.splice(body.id, 1);
+      }
+      res.write(this.getJsonTodoList());
+      res.end();
+    });
+  }
+}
